42b99511d5a76050004753eed2e7d576ce748060
emmaHjJeon <93476644+emmaHjJeon@users.noreply.github.com>
2022-02-13 21:38:22 +0900

    add posts update function

---
+++ src/main/java/com/emmahjjeon/springboot/web/dto/PostsUpdateRequestDto.java
@@ -0,0 +1,19 @@
+package com.emmahjjeon.springboot.web.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.graalvm.compiler.graph.Node;
+
+@Getter
+@NoArgsConstructor
+public class PostsUpdateRequestDto {
+    private String title;
+    private String content;
+
+    @Builder
+    public PostsUpdateRequestDto(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
