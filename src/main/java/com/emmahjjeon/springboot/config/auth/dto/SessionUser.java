5abb81ec9a03f0ae479ec37b5e46753ca32291e9
emmaHjJeon <93476644+emmaHjJeon@users.noreply.github.com>
2022-03-20 23:16:24 +0900

    add authentication function

---
+++ src/main/java/com/emmahjjeon/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,20 @@
+package com.emmahjjeon.springboot.config.auth.dto;
+
+import com.emmahjjeon.springboot.domain.user.User;
+import lombok.Getter;
+
+@Getter
+public class SessionUser {
+
+    private String name;
+
+    private String email;
+
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
